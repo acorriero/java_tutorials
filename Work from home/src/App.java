c7d1020f57c1a515b73cbad5d0b9a6247db6ed07
Anthony Corriero <anthony.corriero@gmail.com>
2014-08-05 01:09:18 -0400

    test

---
+++ Work from home/src/App.java
@@ -0,0 +1,18 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("Hello Happy sales people!");
+		System.out.println("This is app shows sales data");
+		
+	}
+
+}
